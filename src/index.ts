1df235efa52017e330427c496251dc24d0911ce8
suey <yanxu@xiaoduotech.com>
2025-04-08 11:55:32 +0800

    feat: 新建事件总线功能

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 
+export * from './emitter';
+
 export * from './hooks';
 
 export * from './storage';
--- src/storage/cookie.ts
+++ src/storage/cookie.ts
@@ -4,19 +4,19 @@ import Cookies from 'js-cookie';
  * @param {string} key 存储时使用key值
  * @returns {string} 返回存储的数据
  */
-export const ckGet = (key: string) => Cookies.get(key);
+export const ckGet = (key: string): string => Cookies.get(key);
 /**
  * cookie 的 Set 方法
  * @param {string} key 存储时的key值
- * @param {string} value 存储的数据, 可以是一个任意数据类型
- * @param {number} time 存储的时间, 过期则会自动销毁
+ * @param data
+ * @param timer
  * @returns {void}
  */
-export const ckSet = (key: string, data: string, timer?: number) => Cookies.set(key, data, typeof timer === 'number' ? { expires: timer } : void 0);
+export const ckSet = (key: string, data: string, timer?: number): void => Cookies.set(key, data, typeof timer === 'number' ? { expires: timer } : void 0);
 /**
  * cookie 的 Remove 方法
  * @param {string} key 移除key所存储的数据
  * @returns {void}
  */
-export const ckRemove = (key: string) => Cookies.remove(key);
+export const ckRemove = (key: string): void => Cookies.remove(key);
 
